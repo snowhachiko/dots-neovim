e4b7c9e647428e64ed0fecaa5f64798f3c546bb6
snowhachiko <152941020+snowhachiko@users.noreply.github.com>
2024-06-25 07:30:00 +0200

    gitignore not needed, add keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -54,34 +54,34 @@ autocmd('LspAttach', {
 })
 
 -- --========= DAP
--- vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
--- vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
--- vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
--- vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
--- vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
--- vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
--- vim.keymap.set('n', '<Leader>dc', function() require('dap').close() end)
--- vim.keymap.set('n', '<Leader>dt', function() require('dap').terminate() end)
---
--- vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
---     require('dap.ui.widgets').hover()
--- end)
--- vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
---     require('dap.ui.widgets').preview()
--- end)
--- vim.keymap.set('n', '<Leader>df', function()
---     local widgets = require('dap.ui.widgets')
---     widgets.centered_float(widgets.frames)
--- end)
--- vim.keymap.set('n', '<Leader>ds', function()
---     local widgets = require('dap.ui.widgets')
---     widgets.centered_float(widgets.scopes)
--- end)
---
--- -- harpoon
---
---
--- ---- comments
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set('n', '<Leader>dc', function() require('dap').close() end)
+vim.keymap.set('n', '<Leader>dt', function() require('dap').terminate() end)
+
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.scopes)
+end)
+
+----------------------
+------ comments ------
+----------------------
+
 -- -- :help comment.config.Mappings
 --
 -- -- Default comment.nvim mappings
