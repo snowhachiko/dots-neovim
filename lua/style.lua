1fb7f60c083fb33611b6fbca43a6a33c7cdb7206
vilipat <144691423+vilipat@users.noreply.github.com>
2024-11-19 23:53:06 +0100

    fix borders background to be transparent

--- lua/style.lua
+++ lua/style.lua
@@ -11,10 +11,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 local function set_colorscheme(name)
-  local ok, _ = pcall(vim.cmd, 'colorscheme ' .. name)
-  if not ok then
-    vim.notify('Colorscheme ' .. name .. ' not found!', vim.log.levels.WARN)
-  end
+    local ok, _ = pcall(vim.cmd, 'colorscheme ' .. name)
+    if not ok then
+        vim.notify('Colorscheme ' .. name .. ' not found!', vim.log.levels.WARN)
+    end
 end
 
 -- set_colorscheme("onedark")
@@ -38,10 +38,11 @@ vim.g.rustaceanvim = {
     }
 }
 
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
 -- Configure borders for floating windows
 -- vim.opt.pumblend = 0
 -- local float = { focusable = true, style = "minimal", border = "rounded", }
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, float)
 -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, float)
 -- vim.lsp.handlers["textDocument/diagnostics"] = vim.lsp.with(vim.lsp.handlers.diagnostics, float)
-
